89106087972ff4da79b100ba988486c4e8d9c178
Sai Kanala <kandala.sai@gmail.com>
2015-01-08 14:51:16 -0500

    Add songs to playlists

--- app/controllers/playlist_songs_controller.rb
+++ app/controllers/playlist_songs_controller.rb
@@ -4,14 +4,19 @@ class PlaylistSongsController < ApplicationController
   end
 
   def create
-    @playlist_song = PlaylistSong.new
+    @playlist_song = PlaylistSong.new(playlist_song_params)
 
     if @playlist_song.save
       playlist = Playlist.find(@playlist_song.playlist_id)
       redirect_to playlist_url(playlist)
     else
       flash.now[:errors] = @playlist_song.errors.full_messages
-      render: new
+      render json: flash[:errors]
     end
+  end
+
+  private
+  def playlist_song_params
+    params.require(:playlist_song).permit(:song_id, :playlist_id)
   end
 end
