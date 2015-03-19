29478e3da5b47fc3766db3ce93f00797a79f5dc9
Sai Kandala <kandala.sai@gmail.com>
2015-03-19 15:44:24 -0400

    Restyles favorites page

--- app/assets/javascripts/views/user_favorites.js
+++ app/assets/javascripts/views/user_favorites.js
@@ -5,9 +5,39 @@ Songstorm.Views.UserFavorites = Backbone.View.extend({
 		this.listenTo(this.collection, 'sync', this.render);
 	},
 
+	events: {
+		"mouseover .img-item": "showSongModal",
+		"mouseleave .img-item": "hideSongModal"
+	},
+
 	render: function () {
 		var content = this.template({likes: this.collection});
 		this.$el.html(content);
+		this.renderLikes();
 		return this;
+	},
+
+	renderLikes: function () {
+		var container = $("#user-likes");
+		var that = this;
+
+		this.collection.forEach(function (like) {
+			var template = JST["users/like"];
+
+			container.append(template({like: like}));
+		});
+
+	},
+
+	showSongModal: function (event) {
+		event.preventDefault();
+
+		$(event.target).children(".song-modal").fadeIn(150);
+	},
+
+	hideSongModal: function (event) {
+		event.preventDefault();
+
+		$(".song-modal").fadeOut(150);
 	}
 })
